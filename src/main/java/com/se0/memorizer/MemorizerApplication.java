ce5ff9da99c6213410af1be0a95b11e0157c6ac4
seyoung <seyoung0916@naver.com>
2022-06-26 13:08:07 +0900

    init

---
+++ src/main/java/com/se0/memorizer/MemorizerApplication.java
@@ -0,0 +1,13 @@
+package com.se0.memorizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemorizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se0/memorizer/MemorizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se0.memorizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

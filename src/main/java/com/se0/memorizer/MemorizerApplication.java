49a69cfba05c3d779c416d6329971ff775679359
seyoung <seyoung0916@naver.com>
2022-06-26 13:33:59 +0900

    remove HELP.md

--- src/main/java/com/se0/memorizer/MemorizerApplication.java
+++ src/main/java/com/se0/memorizer/MemorizerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MemorizerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MemorizerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MemorizerApplication.class, args);
+	}
 
 }
